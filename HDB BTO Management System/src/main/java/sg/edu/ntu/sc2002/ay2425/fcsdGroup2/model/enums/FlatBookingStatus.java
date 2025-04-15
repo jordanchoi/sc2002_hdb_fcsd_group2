75b87c1eedb891f60ce07b9680b5e6dfacffb5fe
jordanchoi <jcsy11@gmail.com>
2025-04-16 05:39:57 +0800

    Code Cleanup.

--- HDB BTO Management System/src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/model/enums/FlatBookingStatus.java
+++
@@ -1,6 +0,0 @@
-package sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.enums;
-public enum FlatBookingStatus {
-    Available,
-    Booked,
-    Reserved
-}
--- HDB BTO Management System/src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/model/enums/ProjStatus.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.enums;
-public enum ProjStatus{
-    Open,
-    Closed,
-    Booking,
-    Completed
-}
