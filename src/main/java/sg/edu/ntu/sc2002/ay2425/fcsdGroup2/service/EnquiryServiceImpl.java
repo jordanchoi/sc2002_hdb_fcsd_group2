c754d1cdb154c5eb95fe85c16d0c8bd2bf5fe7b4
jordanchoi <jcsy11@gmail.com>
2025-04-23 00:13:36 +0800

    Added skeleton for Enquiry interfaces.

--- src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/service/EnquiryServiceImpl.java
+++ src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/service/EnquiryServiceImpl.java
@@ -1,6 +1,67 @@
 package sg.edu.ntu.sc2002.ay2425.fcsdGroup2.service;
 
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.entities.BTOProj;
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.entities.Enquiry;
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.entities.HDBApplicant;
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.entities.HDBOfficer;
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.repository.EnquiryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class EnquiryServiceImpl implements ApplicantEnquiryService, OfficerEnquiryService, ManagerEnquiryService {
+    // EnquiryRepository enquiryRepository = new EnquiryRepository();
+    // Common Enquiry Service Methods
+    @Override
+    public Optional<Enquiry> getEnquiryById(int enquiryId) {
+        return Optional.empty();
+    }
 
+    @Override
+    public boolean addMessage(int enquiryId, String msg, String senderNric) {
+        return false;
+    }
 
+    // Applicant Enquiry Service Methods
+    @Override
+    public Enquiry submitEnquiry(String message, HDBApplicant applicant, BTOProj proj) {
+        return null;
+    }
+
+    @Override
+    public List<Enquiry> getEnquiriesByApplicant(HDBApplicant applicant) {
+        return List.of();
+    }
+
+    @Override
+    public boolean editOwnMessage(int enquiryId, String msgId, HDBApplicant applicant, String newContent) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteEnquiry(int enquiryId, HDBApplicant applicant) {
+        return false;
+    }
+
+    // Manager Enquiry Service Methods
+    @Override
+    public List<Enquiry> getAllEnquiries() {
+        return List.of();
+    }
+
+    // Officer Enquiry Service Methods
+    @Override
+    public List<Enquiry> getEnquiryForAssignedProj(BTOProj proj) {
+        return List.of();
+    }
+
+    @Override
+    public boolean replyEnquiry(int enquiryId, String reply, HDBOfficer officer) {
+        return false;
+    }
+
+    @Override
+    public List<Enquiry> getEnquiriesByOfficer(HDBOfficer officer) {
+        return List.of();
+    }
 }
--- src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/service/ManagerEnquiryService.java
+++ src/main/java/sg/edu/ntu/sc2002/ay2425/fcsdGroup2/service/ManagerEnquiryService.java
@@ -1,4 +1,7 @@
 package sg.edu.ntu.sc2002.ay2425.fcsdGroup2.service;
+import sg.edu.ntu.sc2002.ay2425.fcsdGroup2.model.entities.Enquiry;
+import java.util.List;
 
 public interface ManagerEnquiryService extends BaseEnquiryService {
+    List<Enquiry> getAllEnquiries();
 }
